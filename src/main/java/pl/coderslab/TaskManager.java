35b0a8ada88ccf54c57189fba4a1d12da26f4c2b
Mirotak <mirekt@interia.pl>
2023-04-28 22:16:06 +0200

    menu

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -15,6 +15,35 @@ public class TaskManager {
     public static void main(String[] args) {
 
 
+     //   Scanner select = new Scanner(System.in);
+        String selectWithMenu = "l";
+        switch (selectWithMenu) {
+//            case 'e':
+//                selectWithMenu = "exit";
+//                //
+//                break;
+//            case 'a':
+//                selectWithMenu = "add";
+//              //  addTask;
+//                break;
+//            case 'r':
+//                selectWithMenu = "remove";
+//                // removeTask;
+//                break;
+            case "l":
+                selectWithMenu = "list";
+                // listTasks;
+                break;
+            default:
+                selectWithMenu = "Please select a correct option . ";
+                break;
+        }
+        System.out.println(selectWithMenu);
+
+
+
+
+
 
 
        /* File file = new File("tasks.csv");
@@ -29,11 +58,6 @@ public class TaskManager {
         }
         System.out.println(reading);
         System.out.println();*/
-
     }
-
-
-
-
 
 }
